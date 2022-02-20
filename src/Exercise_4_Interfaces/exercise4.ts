86ba2fa629233673bb5ad2ed3cbfd311c20e42fd
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2022-02-20 08:32:04 +0100

    Exercise 4 done

--- src/Exercise_4_Interfaces/exercise4.ts
+++ src/Exercise_4_Interfaces/exercise4.ts
@@ -12,20 +12,28 @@ export default () => {
   // Instructions:
   // • Create an interface `CartItem` and replace the param's type with it
   // • Make variantId optional
+  interface CartItem {
+    id: number,
+    title: string,
+    variantId?: number
+  }
 
-  function addToCart(item: {id: number, title: string, variantId: number}) {
+  function addToCart(item: CartItem) {
     console.log('[Exercise 4.1]', `Adding "${item.title}" to cart.`);
   }
 
-  addToCart({id: 1, title: 'Concrete shoes'});
+  addToCart({ id: 1, title: 'Concrete shoes' });
 
   // ======== Exercise 4.2 ========
   // Instructions:
   // • Create and implement an interface on `Person` to ensure it always has accessible
   //   `name` and `age` member properties.
-
-  class Person {
-    constructor(public name: string, public age: number) {}
+  interface Person {
+    name: string,
+    age: number
+  }
+  class Person implements Person {
+    constructor(public name: string, public age: number) { }
   }
 
   const jane = new Person('Jane', 31);
@@ -41,10 +49,17 @@ export default () => {
 
   // [do not edit] (pretend this is coming from external `foo.d.ts` lib)
   interface City {
-    name: string;
+    name:string;
   }
+  interface City {
+    coords:Coords;
+  }
+  
   // [/do not edit]
-
+  interface Coords {
+    latitude:number;
+    longitude:number;
+  }
   const montreal = {
     coords: {
       latitude: 42.332,
@@ -55,14 +70,14 @@ export default () => {
 
   const tampa = {
     coords: {
-      latitude: '27.9478',
-      longitude: '-82.4584',
+      latitude: 27.9478,
+      longitude: -82.4584,
     },
     name: 'Tampa',
   };
 
   function getCityInfo(city: City) {
-    const coords = `(${city.coords.latitude.toFixed(3) }, ${city.coords.longitude.toFixed(3) })`;
+    const coords = `(${city.coords.latitude.toFixed(3)}, ${city.coords.longitude.toFixed(3)})`;
     return `${city.name.toUpperCase()} is located at ${coords}.`;
   }
 
@@ -77,7 +92,7 @@ export default () => {
   }
 
   class User implements UserSchema {
-    constructor(public name: string, readonly id: number) {}
+    constructor(public name: string, readonly id: number) { }
   }
 
   const user = new User('Dog', 1);
@@ -85,7 +100,7 @@ export default () => {
   console.log(user.id); // readable
 
   user.name = 'Harold'; // writable
-  user.id = 5; // not writable
+  // user.id = 5; // not writable
 
   console.log(`User:`, user)
 }
